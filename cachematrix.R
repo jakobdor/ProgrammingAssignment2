1240817605de0ad408c78ca2d6c80638ceb68131
Jakob Dordevic <jakob.dordevic@googlemail.com>
2018-01-24 12:07:55 +0100

    Let's do it!

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,55 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function creates a special "matrix" object that can cache its inverse.
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(mat = matrix()) {
+  inv <- NULL
+  set <- function(matrix ) {
+    mat <<- matrix
+    inv <<- NULL
+  }
+  get <- function() {
+    mat
+  }
+  setInverse <- function(inverse) {
+    inv <<- inverse
+  }
+  getInverse <- function() {
+    inv
+  }
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
+##This function computes the inverse of the special "matrix"
+##returned by makeCacheMatrix above. 
+##If the inverse has already been calculated (and the matrix has not changed),
+##then the cachesolve should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  mat <- x$getInverse()
+  if(!is.null(mat)) {
+    message("getting cached data")
+    return(mat)
+  }
+  data <- x$get()
+  mat <- solve(data) %*% data
+  x$setInverse(mat)
+  mat
 }
+
+##Let's see if this works.
+x <- rbind(c(1, 2, 3), c(0.5, 6, 9.9))
+mat <- makeCacheMatrix(x)
+cacheSolve(mat)
+cacheSolve(mat)
+
+##Error in solve.default(data) : 'a' (2 x 3) must be square
+##-> let's try again with a square matrix
+
+x <- rbind(c(1, 2, 3), c(0.5, 6, 9.9), c(0.5, 0.1, 4))
+mat <- makeCacheMatrix(x)
+cacheSolve(mat)
+cacheSolve(mat)
+
+##Perfect!
